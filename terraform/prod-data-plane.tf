686b515b233ae8b2d8f50ed0404524e38a71dbc2
Filipe Pais Lenfers <filipelenfers@gmail.com>
2019-02-22 20:32:19 -0300

    added the existing ssm parameters to terraform

--- terraform/prod-data-plane.tf
+++ terraform/prod-data-plane.tf
@@ -1,3 +1,7 @@
+variable "environment" {
+  type = "string"
+  default = "dev"
+}
 
 provider "aws" {
   region     = "us-east-1"
@@ -40,4 +44,64 @@ resource "aws_dynamodb_table_item" "dev-quickstart" {
   }
 }
 ITEM
+}
+
+resource "aws_ssm_parameter" "secret" {
+  name        = "/${var.environment}/jwt/algorithm/secret"
+  description = "Secret for hcma"
+  type        = "SecureString"
+  value       = "2BA7F56A04BA587C19F456D92AB04ADE5FBF5BA933F809B76B0C1346384B2602"
+  overwrite   = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_ssm_parameter" "token_audience" {
+  name        = "/${var.environment}/jwt/audience"
+  description = "Audience to be set on jwt token"
+  type        = "StringList"
+  value       = "DevApis,BetaApis"
+  overwrite   = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_ssm_parameter" "token_expiration" {
+  name        = "/${var.environment}/jwt/expirationInMinutes"
+  description = "Expiration in minutos for the jwt token"
+  type        = "String"
+  value       = "60"
+  overwrite   = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_ssm_parameter" "token_issuer" {
+  name        = "/${var.environment}/jwt/issuer"
+  description = "Token issuer"
+  type        = "SecureString"
+  value       = "lenferslab"
+  overwrite   = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
+
+resource "aws_ssm_parameter" "token_nb" {
+  name        = "/${var.environment}/jwt/nbSeconds"
+  description = "Not before secons for the token"
+  type        = "String"
+  value       = "30"
+  overwrite   = true
+
+  tags = {
+    environment = "${var.environment}"
+  }
 }
